9d89aecb76feaac28c1ef96bc65c13953a222a46
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-14 10:26:44 +0200

    Ex 7

--- Program.cs
+++ Program.cs
@@ -1,112 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Security.Cryptography.X509Certificates;
 
 // Aufgabe 1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus
-
-int a = 10;
-Console.WriteLine(a);
-
-int b = 20;
-Console.WriteLine(b);
-
-int summe = a + b;
-Console.WriteLine(summe);
-
-// Aufgabe 2: Lasse den Nutzer zwei Zahlen eingeben und gebe die Summe auf der Console aus:
-
-Console.WriteLine("Gebe uns deine erste Zahl!");
-string eingabe1 = Console.ReadLine();
-
-//Frage den Nutzer nach der zweiten Zahl
-Console.WriteLine("Gebe uns deine zweite Zahl!");
-string eingabe2 = Console.ReadLine();
-
-//Gebe das Ergebnis auf der Console aus
-int zahl1 = Convert.ToInt16(eingabe1);
-int zahl2 = Convert.ToInt16(eingabe2);
-
-int ergebnis = zahl1 + zahl2;
-
-Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
-
-
-//Aufgabe 3: Lasse den Nutzer eine Zahl eingeben und gebe alle Zahlen bis einschließlich der eingegeben Zahl aus
-
-Console.WriteLine("Gebe uns deine Zahl!");
-string eingabe3 = Console.ReadLine();
-
-int zahl3 = Convert.ToInt16(eingabe3);
-int i = 0;
-
-while (i <= zahl3)
-{
-    Console.WriteLine(i);
-    i++;
-}
-//Aufgabe 4: Lasse den Nutzer eine Zahl eingeben und gebe nur die geraden Zahlen aus.
-
-Console.WriteLine("Gebe uns deine Zahl!");
-string eingabe4 = Console.ReadLine();
-
-int zahl4 = Convert.ToInt16(eingabe4);
-int zahlx = 0;
-
-while (zahlx <= zahl4)
-{
-    if (zahlx % 2 == 0)
+namespace Example{
+    class Programm
     {
-        Console.WriteLine(zahlx);
+        static void Main(string[] args)
+        {
+        //Aufgabe 7: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“
+        Console.WriteLine("Schreib etwas!");
+        string eingabe = Console.ReadLine();
+        
+        Console.WriteLine(eingabe.Replace('U', 'G').Replace('u', 'g'));
+        }
     }
-    zahlx++;
-}
-//Aufgabe 5: Lasse den Nutzer eine Zahl eingeben und berechne von dieser Zahl die Fakultät.
-
-Console.WriteLine("Gebe uns deine Zahl!");
-string eingabe5 = Console.ReadLine();
-
-int zahl5 = Convert.ToInt16(eingabe5);
-int summes = 1;
-
-for (int zahls = 1; zahls <= zahl5; zahls++)
-{
-    summes = summes*zahls;
-    Console.WriteLine(summes);
 }
 
-//Aufgabe 6a: Lasse den Nutzer einen Text eingeben und gebe den Text nur in Großbuchstaben aus
-
-Console.WriteLine("Schreib etwas!");
-string eingabe6 = Console.ReadLine();
-
-Console.WriteLine(eingabe6.ToUpper());
-
-//Aufgabe 6b: Gebe die Länge des Textes aus
-
-Console.WriteLine("Schreib etwas!");
-string eingabe7 = Console.ReadLine();
-
-Console.WriteLine("The length of the text is: " + eingabe7.Length);
-
-//Aufgabe 6c: Zähle wie oft der Buchstabe a vorkommt.
-
-Console.WriteLine("Schreib etwas!");
-string eingabe8 = Console.ReadLine();
-char ch = 'a';
-
-int freq = eingabe8.Split(ch).Length - 1;
-Console.WriteLine(freq);
-
-//Aufgabe 6d: Gebe nur einen Teil des Textes aus. Ab Position 3, und dann die nächsten 5 Buchstaben
-
-Console.WriteLine("Schreib etwas!");
-string eingabe9 = Console.ReadLine();
-
-if (eingabe9.Length < 9)
-{
-    Console.WriteLine("Scheib etwas noch!");
-}
-else
-{
-    Console.WriteLine(eingabe9.Substring(2, 5));
-}
 
