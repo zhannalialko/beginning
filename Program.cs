c6119fa7a16490de1c2384f5ef55a5cdd622af4b
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-14 11:20:02 +0200

    ex 9

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Security.AccessControl;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
 // Aufgabe 1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus
 namespace Example{
@@ -9,12 +11,23 @@ namespace Example{
     {
         static void Main(string[] args)
         {
-        //Aufgabe 8: Frage den Nutzer nach einer Texteingabe und fülle diese mit bis zu 20 Stellen mit einem y von links auf
+        //Aufgabe 9: Frage den Nutzer nach einer Eingabe mit Semikolons ( ; ) und Splitte die Eingabe an diesem Zeichen. Gebe die einzelnen Werte auf der Console aus.
 
-        Console.WriteLine("Schreib etwas");
+        Console.WriteLine("Schreib etwas mit ;");
         string eingabe = Console.ReadLine();
+        
+            while(eingabe.Contains(';') == false)
+            {
+                Console.WriteLine("Schreib bitte etwas mit ;");
+                eingabe = Console.ReadLine();
+            }
+        string[] ergebnis = eingabe.Split(";");
 
-        Console.WriteLine(eingabe.PadLeft(20, 'y'));
+        Console.Write("Ergebnis: ");
+        foreach(string str in ergebnis)
+        {
+            Console.WriteLine(str + "\n");
+        }    
         }
     }
 }
