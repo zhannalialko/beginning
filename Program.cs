84cafa0370e90069992d6a38876d41c23235f2cc
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-15 09:42:42 +0200

    mathode advanced ex

--- Program.cs
+++ Program.cs
@@ -11,6 +11,85 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+        Console.WriteLine("Schreib eine Zahl");
+        string numero1 = Console.ReadLine();
+
+        Console.WriteLine("Schreib andere Zahl");
+        string numero2 = Console.ReadLine();
+
+        string operators ="";
+
+        do
+        {
+            Console.WriteLine("Schreib den operator (+, -, *, / oder %)");
+            operators = Console.ReadLine();
+        } while(operators != "+" && operators != "-" && operators != "*" && operators != "/" && operators != "%");
+        
+        switch(operators)
+        {
+            case "+":
+                int ergebnis = AdditionWithReturn(numero1, numero2);
+                break;
+            case "-":
+                Substract(numero1, numero2);
+                break;
+            case "*":
+                Multiplikation(numero1, numero2);
+                break;
+             case "/":
+                Division(numero1, numero2);
+                break;
+            case "%":
+                Teil(numero1, numero2);
+                break;
+            default:
+                Console.WriteLine("Ich kenne diesen Operator nicht");
+                 break;
+        }
+
+        }
+        static int AdditionWithReturn(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            return numero1 + numero2;
+        }
+        
+        static void Addition(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 + numero2);
+        }
+        
+        static void Substract(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 - numero2);
+        }
+        
+        static void Multiplikation(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 * numero2);
+        }
+
+        static void Division(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 / numero2);
+        }
+        static void Teil(string zahl1, string zahl2)
+        {
+            int numero1 = Convert.ToInt32(zahl1);
+            int numero2 = Convert.ToInt32(zahl2);
+            Console.WriteLine(numero1 % numero2);
+        }
+
         static void Addition(int zahl1, int zahl2)
         {
             Console.WriteLine(zahl1 + zahl2);
@@ -30,42 +109,6 @@ namespace HelloWorld
         static void Teil(int zahl1, int zahl2)
         {
             Console.WriteLine(zahl1 % zahl2);
-        }
-        
-        
-        int a = 10;
-        Console.WriteLine(a);
-        int b = 20;
-        Console.WriteLine(b);
-        string operators ="";
-
-        do
-        {
-            Console.WriteLine("Schreib den operator (+, -, *, / oder %)");
-            operators = Console.ReadLine();
-        } while(operators != "+" && operators != "-" && operators != "*" && operators != "/" && operators != "%");
-        
-        switch(operators)
-        {
-            case "+":
-                Addition(a, b);
-                break;
-            case "-":
-                Substract(a, b);
-                break;
-            case "*":
-                Multiplikation(a, b);
-                break;
-             case "/":
-                Division(a, b);
-                break;
-            case "%":
-                Teil(a, b);
-                break;
-            default:
-                Console.WriteLine("Ich kenne diesen Operator nicht");
-                 break;
-        }
         }
         
     }
