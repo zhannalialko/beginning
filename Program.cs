04a251441954387670fc868015cf6592530af9a0
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-15 09:13:18 +0200

    methode ex

--- Program.cs
+++ Program.cs
@@ -11,41 +11,63 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            Console.WriteLine(a);
-            int b = 20;
-            Console.WriteLine(b);
-            string operators ="";
-
-            do
-            {
-                Console.WriteLine("Schreib den operator (+, -, *, / oder %)");
-                operators = Console.ReadLine();
-            } while(operators != "+" && operators != "-" && operators != "*" && operators != "/" && operators != "%");
-
-            switch(operators)
-            {
-                case "+":
-                    Console.WriteLine(a + b);
-                    break;
-                case "-":
-                    Console.WriteLine(a - b);
-                    break;
-                case "*":
-                    Console.WriteLine(a * b);
-                    break;
-                case "/":
-                    Console.WriteLine(a / b);
-                    break;
-                case "%":
-                    Console.WriteLine(a % b);
-                    break;
-                default:
-                    Console.WriteLine("Ich kenne diesen Operator nicht");
-                    break;
-
-            }
+        static void Addition(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1 + zahl2);
         }
+        static void Substract(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1 - zahl2);
+        }  
+        static void Multiplikation(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1 * zahl2);
+        }  
+        static void Division(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1 / zahl2);
+        }
+        static void Teil(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1 % zahl2);
+        }
+        
+        
+        int a = 10;
+        Console.WriteLine(a);
+        int b = 20;
+        Console.WriteLine(b);
+        string operators ="";
+
+        do
+        {
+            Console.WriteLine("Schreib den operator (+, -, *, / oder %)");
+            operators = Console.ReadLine();
+        } while(operators != "+" && operators != "-" && operators != "*" && operators != "/" && operators != "%");
+        
+        switch(operators)
+        {
+            case "+":
+                Addition(a, b);
+                break;
+            case "-":
+                Substract(a, b);
+                break;
+            case "*":
+                Multiplikation(a, b);
+                break;
+             case "/":
+                Division(a, b);
+                break;
+            case "%":
+                Teil(a, b);
+                break;
+            default:
+                Console.WriteLine("Ich kenne diesen Operator nicht");
+                 break;
+        }
+        }
+        
     }
 }
 
