44c2135101046b73c0444a3662ba578a9474b370
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-18 15:11:59 +0200

    rückzahlung ex

--- Program.cs
+++ Program.cs
@@ -13,26 +13,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Wie viel Zahlen möchtest du in einem Array eingeben?");
+           Console.WriteLine("Gib eine Zahl");
            int eingabe = Convert.ToInt32(Console.ReadLine());
 
-           int[] numbers = new int[eingabe];
-
-           for(int i = 0; i < numbers.Length; i++)
-           {
-                Console.WriteLine("Gib eine Zahl");
-                int zahl = Convert.ToInt32(Console.ReadLine());
-                numbers[i] = zahl;
-
-           }
-            int klein = numbers.Min();
-            int groß = numbers.Max();
-            double mitte = numbers.Average();
             
-            Console.WriteLine($"Die kleinste Zahl ist {klein}");
-            Console.WriteLine($"Die größte Zahl ist {groß}");
-            Console.WriteLine($"Der Durchschnitt ist {mitte}");
-
+            for(; eingabe>0; eingabe--)
+            {
+                Console.WriteLine(eingabe);
+            }
         }
 
     }
