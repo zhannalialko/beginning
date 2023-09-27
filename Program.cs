05bd4a2d66359ae7d29023000522b54f0b9c40d4
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-27 15:38:54 +0200

    aufgabe 13

--- Program.cs
+++ Program.cs
@@ -19,42 +19,27 @@ using System.Net.Http.Headers;
 using System.Xml.Schema;
 using System.Security.Cryptography;
 using System.Collections.Immutable;
+using System.Runtime.InteropServices;
 namespace HelloWorld
 {
     class Programm
     {
         static void Main(string[] args)
         {  
-            List<Students> stulist = new List<Students>(); 
-            stulist.Add(new Students { StuId = 1, StuName = " Joseph ", GrPoint = 800 });
-            stulist.Add(new Students { StuId = 2, StuName = "Alex", GrPoint = 458 }); 
-            stulist.Add(new Students { StuId = 3, StuName = "Harris", GrPoint = 900 }); 
-            stulist.Add(new Students { StuId = 4, StuName = "Taylor", GrPoint = 900 }); 
-            stulist.Add(new Students { StuId = 5, StuName = "Smith", GrPoint = 458 }); 
-            stulist.Add(new Students { StuId = 6, StuName = "Natasa", GrPoint = 700 }); 
-            stulist.Add(new Students { StuId = 7, StuName = "David", GrPoint = 750 }); 
-            stulist.Add(new Students { StuId = 8, StuName = "Harry", GrPoint = 700 }); 
-            stulist.Add(new Students { StuId = 9, StuName = "Nicolash", GrPoint = 597 });
-            stulist.Add(new Students { StuId = 10, StuName = "Jenny", GrPoint = 750 });
+            string[] arr1 = { "aaa.frx", "bbb.TXT", "xyz.dbf","abc.pdf",
+             "aaaa.PDF","xyz.frt", "abc.xml", "ccc.txt", "zzz.txt" };
+            
+            
+            var arr2 = arr1.Select(s => s.Split('.')[1].ToLower()).GroupBy(g => g);
+            
 
-            Console.WriteLine("Schreib den Zahl von 1 bis 6");
-            int zahl = Convert.ToInt16(Console.ReadLine()); 
-            var groups = stulist.OrderByDescending(g => g.GrPoint)
-                                .GroupBy(g => g.GrPoint).ToList();
-
-            var entry = groups[zahl-1];
-            foreach(Students s in entry)
+            foreach(var item in arr2)
             {
-                Console.WriteLine(s.StuName);
-            }          
+                Console.WriteLine($"{item.Count()} File(s) with {item.Key} Extension");
+            }
+                      
         } 
-          
-        public class Students 
-        { 
-            public string StuName { get; set; } 
-            public int GrPoint { get; set; } 
-            public int StuId { get; set; } 
-        } 
+         
              
     }
 }
