f1506b439307e7c9d58bc13faa8a76a6dafa5257
TEC\ZLIA <Zhanna.Lialko@tecalliance.net>
2023-09-27 12:53:00 +0200

    aufgabe 5

--- Program.cs
+++ Program.cs
@@ -23,10 +23,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {   
-            int[] arr1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
-            foreach(var item in arr1.GroupBy(x => x))
+            string str = "w3resource";
+            
+            foreach(var item in str.GroupBy(x => x))
             {
-                Console.WriteLine($"Die Zahl {item.Key} entsteht {item.Count()} mal");
+                Console.WriteLine($"Das Zeichen {item.Key} entsteht {item.Count()} mal");
             }
 
         } 
